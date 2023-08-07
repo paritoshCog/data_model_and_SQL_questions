d7f4bb5f37d34919ac6bfaa0388c069e99445ee1
Paritosh Prasad <128465174+paritoshCog@users.noreply.github.com>
2023-08-07 17:22:59 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -2,26 +2,26 @@
   
 select * from movie 
 join
-	rating ON movie.mov_id = rating.mov_id
+rating ON movie.mov_id = rating.mov_id
 join
-	movie_genres ON rating.mov_id = movie_genres.mov_id
+movie_genres ON rating.mov_id = movie_genres.mov_id
 join
-	genres ON movie_genres.gen_id = genres.gen_id
+genres ON movie_genres.gen_id = genres.gen_id
 where
-	genres.gen_title = 'comedy'
+genres.gen_title = 'comedy'
 ordered by 
-	rating.num_o_ratings DESC
+rating.num_o_ratings DESC
 
 -- 2.	All the movies Directed by “Steven Spielberg”.
 
 
 select * from movie
 join
-	movie_direction ON movie.mov_id = movie_direction.mov_id
+movie_direction ON movie.mov_id = movie_direction.mov_id
 join
-	director ON movie_direction.dir_id = director.dir_id
-having
-	director.dirfname = 'Steven' and director.dirlname = 'Spielberg'   
+director ON movie_direction.dir_id = director.dir_id
+where
+director.dirfname = 'Steven' and director.dirlname = 'Spielberg'   
 	
 -- 3.	All the movies where “Samuel L. Jackson” acted as “Supporting Actor” role.
 
@@ -30,8 +30,9 @@ join
 movie_cast on movie.mov_id = movie_cast.mov_id
 join
 actor ON movie_cast.act_id = actor.act_id
-having
-movie_cast.role = 'Supporting Actor' and CONCAT(actor.act_fname, actor.act_lname) = 'Samuel L.Jackson'
+where
+movie_cast.role = 'Supporting Actor' and actor.act_fname = 'Samuel L.' and actor.act_lname = 'Jackson'
+
 
 
 
